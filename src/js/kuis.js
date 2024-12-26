5f8c7b2e88161fe6aacc15fe2428c58ba73fdaf0
Abil <83733696+Abil23@users.noreply.github.com>
2024-12-26 16:41:26 +0700

    update

--- src/js/kuis.js
+++ src/js/kuis.js
@@ -26,7 +26,7 @@ const startQuiz = () => {
   //   cat = category.value,
   //   diff = difficulty.value;
   loadingAnimation();
-  const url = `/life/soal.html`;
+  const url = `/soal.html`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
